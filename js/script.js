7c82aa6091e3f355c0799fe66575a3fa98138a2a
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-10-23 22:40:32 +0500

    actual information

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var x = document.getElementById("third");
+x.style.display = "none";
+
+var showBtn = document.getElementById("showButton");
+var closeBtn = document.getElementById("closeButton");
+
+function showAll() {
+	if(x.style.display == "none") {
+		x.style.display = "block";
+		closeBtn.style.display = "block";
+		showBtn.style.display = "none";
+	} else {
+		x.style.display = "none";
+		closeBtn.style.display = "none";
+		showBtn.style.display = "block";
+	}
+}
