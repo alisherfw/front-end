f3c5eb9c1c2768acd363234975f2b4b260a0819f
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-10-26 16:37:06 +0500

    html formatted

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ var showBtn = document.getElementById("showButton");
 var closeBtn = document.getElementById("closeButton");
 
 function showAll() {
-	if(x.style.display == "none") {
-		x.style.display = "block";
-		closeBtn.style.display = "block";
-		showBtn.style.display = "none";
-	} else {
-		x.style.display = "none";
-		closeBtn.style.display = "none";
-		showBtn.style.display = "block";
-	}
+    if (x.style.display == "none") {
+        x.style.display = "block";
+        closeBtn.style.display = "block";
+        showBtn.style.display = "none";
+    } else {
+        x.style.display = "none";
+        closeBtn.style.display = "none";
+        showBtn.style.display = "block";
+    }
 }
